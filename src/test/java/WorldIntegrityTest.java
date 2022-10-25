ccb7bcddd0e714f61278f7465bab6469a9f8407c
Yopciem <maciekkopec007@gmail.com>
2022-10-25 11:47:23 +0200

    lab4 - test results comparing improvement

--- src/test/java/WorldIntegrityTest.java
+++ src/test/java/WorldIntegrityTest.java
@@ -6,47 +6,71 @@ import static org.junit.jupiter.api.Assertions.*;
 public class WorldIntegrityTest {
     @Test
     void NormalCaseTest() {
-        String [] args = {"r","l","f","f","f","b"};
+        String[] args = {"r", "l", "f", "f", "f", "b"};
         MoveDirection[] directions = OptionsParser.parse(args);
 
         // case without collision
         IWorldMap map = new RectangularMap(5, 5);
-        Vector2d[] positions = { new Vector2d(2,3), new Vector2d(2,1), new Vector2d(2,2)};
+        Vector2d[] positions = {new Vector2d(2, 3), new Vector2d(2, 1), new Vector2d(2, 2)};
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+        String result = new MapVisualizer(map).draw(new Vector2d(0, 0), new Vector2d(4, 4));
+        assertEquals(
+                parseString("""
+                         y\\x  0 1 2 3 4
+                          5: -----------
+                          4: | | | | | |
+                          3: | | | |>| |
+                          2: | | | | | |
+                          1: | |<|^| | |
+                          0: | | | | | |
+                         -1: -----------
+                        """),parseString(result)
+        );
+
+    }
+
+    @Test
+    void CollisionCaseTest() {
+        // case with 2 collisions and 1 animal which cannot be placed
+        String [] args = new String[]{"f","l","r","f","f","f"};
+        MoveDirection [] directions = OptionsParser.parse(args);
+        IWorldMap map = new RectangularMap(5, 5);
+        Vector2d[] positions = new Vector2d[]{new Vector2d(2, 2), new Vector2d(2, 1), new Vector2d(2, 3), new Vector2d(2, 2)};
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
         String result = new MapVisualizer(map).draw(new Vector2d(0,0),new Vector2d(4,4));
         assertEquals(
-                """
-                         y\\x  0 1 2 3 4\r
-                          5: -----------\r
-                          4: | | | | | |\r
-                          3: | | | |>| |\r
-                          2: | | | | | |\r
-                          1: | |<|^| | |\r
-                          0: | | | | | |\r
-                         -1: -----------\r
-                        """,result
+                parseString("""
+                          4: | | | | | |
+                          3: | | | |>| |
+                          2: | | |^| | |
+                          1: | |<| | | |
+                          0: | | | | | |
+                        """),parseString(result)
         );
+    }
 
+    @Test
+    void ExerciseCaseTest() {
         // case with 2 collisions and 1 animal which cannot be placed
-        args = new String[]{"f","l","r","f","f","f"};
-        directions = OptionsParser.parse(args);
-        map = new RectangularMap(5, 5);
-        positions = new Vector2d[]{new Vector2d(2, 2), new Vector2d(2, 1), new Vector2d(2, 3), new Vector2d(2, 2)};
-        engine = new SimulationEngine(directions, map, positions);
+        String [] args = {"f", "b","r","l","f", "f", "r", "r","f", "f", "f", "f", "f", "f", "f", "f"};
+        MoveDirection [] directions = OptionsParser.parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions =  { new Vector2d(2,2), new Vector2d(3,4) };
+        IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        result = new MapVisualizer(map).draw(new Vector2d(0,0),new Vector2d(4,4));
+        String result = new MapVisualizer(map).draw(new Vector2d(0,0),new Vector2d(9,4));
         assertEquals(
-                """
-                         y\\x  0 1 2 3 4\r
-                          5: -----------\r
-                          4: | | | | | |\r
-                          3: | | | |>| |\r
-                          2: | | |^| | |\r
-                          1: | |<| | | |\r
-                          0: | | | | | |\r
-                         -1: -----------\r
-                        """,result
+                parseString("""
+                         
+                           | | | |^| | | | | | |
+                           | | | | | | | | | | |
+                           | | | | | | | | | | |
+                           | | | | | | | | | | |
+                           | | |˅| | | | | | | |
+                         
+                        """),parseString(result)
         );
     }
 
@@ -62,17 +86,21 @@ public class WorldIntegrityTest {
         engine.run();
         String result = new MapVisualizer(map).draw(new Vector2d(0,0),new Vector2d(1,1));
         assertEquals(
-                """
-                         y\\x  0 1\r
-                          2: -----\r
-                          1: | | |\r
-                          0: |>| |\r
-                         -1: -----\r
-                        """,result
+                parseString("""
+                          
+                         | | |
+                         |>| |
+                         
+                        """),parseString(result)
         );
     }
 
+    private String parseString(String s)
+    {
+        return s.replaceAll("[^|><^˅]+", "");
+    }
 }
+
 
 //    String [] args = {"","","",""};
 //    MoveDirection[] directions = OptionsParser.parse(args);
